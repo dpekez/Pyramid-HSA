52b5ee9871951ef112a84f5d4ad7de6c5e4ab391
Dejan Pekez <dejan@pekez.de>
2019-02-12 23:28:23 +0100

    first rough events view layouting

---
+++ Pyramid HSA/Events/EventCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  EventCollectionViewCell.swift
+//  Pyramid HSA
+//
+//  Created by Dejan Pekez on 12.02.19.
+//  Copyright © 2019 Dejan Pekez. All rights reserved.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureCell()
+    }
+    
+    private func configureCell() {
+        self.layer.cornerRadius = 10
+        self.layer.masksToBounds = true
+    }
+}
