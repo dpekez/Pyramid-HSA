5ca2ce59a6f06443b42d766dad0242a513cd21e8
Dejan Pekez <dejan@pekez.de>
2019-02-21 15:45:44 +0100

    homeview icons now hotlink to homepages

--- Pyramid HSA/Home/HomeViewController.swift
+++ Pyramid HSA/Home/HomeViewController.swift
@@ -11,11 +11,33 @@ import UIKit
 class HomeViewController: UIViewController {
     @IBOutlet weak var countDownCircleView: CountdownCircleView!
     
+    @IBAction func pyramidHeader(_ sender: Any) {
+        openURL(string: "https://www.pyramid-hsa.de")
+    }
+    @IBAction func hsaIcon(_ sender: Any) {
+        openURL(string: "https://www.hs-augsburg.de")
+    }
+    
+    @IBAction func fbIcon(_ sender: Any) {
+        openURL(string: "https://www.facebook.com/Firmenkontaktmesse.Pyramid/")
+    }
+    
+    @IBAction func igIcon(_ sender: Any) {
+        openURL(string: "https://www.instagram.com/pyramid_hsa/")
+    }
+    
     @IBAction func partnerViewUnwindAction(unwindSegue: UIStoryboardSegue) {
     }
+    
+    @IBOutlet weak var partnerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         countDownCircleView.create()
+        partnerButton.setTitle("Unsere\nPartner", for: .normal)
+    }
+    
+    private func openURL(string: String) {
+        UIApplication.shared.open(URL(string: string)!, options: [:], completionHandler: nil)
     }
 }
