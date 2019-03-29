92f91faf3a08c6342d70dc3dbc00098dca2a9a2e
Dejan Pekez <dejan@pekez.de>
2019-03-29 12:28:35 +0100

    adjusted homeview to countdowncircle refactor

--- Pyramid HSA/Home/HomeViewController.swift
+++ Pyramid HSA/Home/HomeViewController.swift
@@ -11,6 +11,9 @@ import MapKit
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var countdownCircle: CountdownCircleView!
+    private var timer = Timer()
+    private let countdown = Countdown()
     
     @IBAction func pyramidHeader(_ sender: Any) {
         openURL(string: "https://www.pyramid-hsa.de")
@@ -27,9 +30,28 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         partnerButton.setTitle("Unsere\nPartner", for: .normal)
+        
+        if countdown.eventIsCommingUp() {
+            runTimer()
+        } else {
+            countdownUpdate()
+        }
     }
     
     private func openURL(string: String) {
         UIApplication.shared.open(URL(string: string) ?? URL(string: "https://www.duck.com")!, options: [:], completionHandler: nil)
+    }
+    
+    private func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(self.countdownUpdate)), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func countdownUpdate() {
+        if !countdown.eventIsCommingUp() {
+            timer.invalidate()
+            countdownCircle.removeFromSuperview()
+        } else {
+            countdownCircle.updateProgress()
+        }
     }
 }
