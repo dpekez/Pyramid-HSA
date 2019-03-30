21db5fd16430ad4328572a837be79601c5b5b87e
Dejan Pekez <dejan@pekez.de>
2019-03-30 16:37:04 +0100

    refactored map overlay

--- Pyramid HSA/Util/MapViewOverlays.swift
+++ Pyramid HSA/Util/MapViewOverlays.swift
@@ -10,50 +10,92 @@ import UIKit
 import MapKit
 
 class MapViewOverlays: MKMapView, MKMapViewDelegate {
-
     override func awakeFromNib() {
         delegate = self
-        createPointAnnotation()
         createOverlay()
     }
-
-    func createPointAnnotation() {
-        let annotationM = MKPointAnnotation()
-        let annotationL = MKPointAnnotation()
-        let annotationTentA = MKPointAnnotation()
-        let annotationTentB = MKPointAnnotation()
-        let annotationW = MKPointAnnotation()
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolygon {
+            let polygonRenderer = MKPolygonRenderer(overlay: overlay)
+            polygonRenderer.fillColor = PyramidColor.pyramidBrighterBlue.withAlphaComponent(0.3)
+            polygonRenderer.strokeColor = PyramidColor.pyramidDarkBlue
+            polygonRenderer.lineWidth = 1
+            
+            return polygonRenderer
+            
+        } else if overlay is MKPolyline {
+            let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = PyramidColor.pyramidDarkBlue
+            polylineRenderer.lineWidth = 3
+            
+            return polylineRenderer
+        }
         
-        annotationM.coordinate = CLLocationCoordinate2D(latitude: 48.358564, longitude: 10.906355)
-        annotationL.coordinate = CLLocationCoordinate2D(latitude: 48.358908, longitude: 10.905958)
-        annotationTentA.coordinate = CLLocationCoordinate2D(latitude: 48.358602, longitude: 10.905878)
-        annotationTentB.coordinate = CLLocationCoordinate2D(latitude: 48.358860, longitude: 10.906377)
-        annotationW.coordinate = CLLocationCoordinate2D(latitude: 48.358496, longitude: 10.907053)
-        
-        addAnnotation(annotationM)
-        addAnnotation(annotationL)
-        addAnnotation(annotationTentA)
-        addAnnotation(annotationTentB)
-        addAnnotation(annotationW)
+        return MKOverlayRenderer()
     }
     
-    func createOverlay() {
-        let coordinatesLM = [
-            CLLocationCoordinate2D(latitude: 48.358757, longitude: 10.905410),
-            CLLocationCoordinate2D(latitude: 48.359293, longitude: 10.906501),
-            CLLocationCoordinate2D(latitude: 48.358878, longitude: 10.907143),
-            CLLocationCoordinate2D(latitude: 48.358214, longitude: 10.905982),
-            CLLocationCoordinate2D(latitude: 48.358347, longitude: 10.905852),
-            CLLocationCoordinate2D(latitude: 48.358917, longitude: 10.906835),
-            CLLocationCoordinate2D(latitude: 48.359144, longitude: 10.906567),
-            CLLocationCoordinate2D(latitude: 48.358652, longitude: 10.905508)
+    func createPointAnnotation() {
+        let annotationL = MKPointAnnotation()
+        let annotationM = MKPointAnnotation()
+        let annotationW = MKPointAnnotation()
+        let annotationTentA = MKPointAnnotation()
+        let annotationTentB = MKPointAnnotation()
+        
+        annotationL.coordinate = CLLocationCoordinate2D(latitude: 48.358908, longitude: 10.905958)
+        annotationM.coordinate = CLLocationCoordinate2D(latitude: 48.358564, longitude: 10.906355)
+        annotationW.coordinate = CLLocationCoordinate2D(latitude: 48.358496, longitude: 10.907053)
+        annotationTentA.coordinate = CLLocationCoordinate2D(latitude: 48.358602, longitude: 10.905878)
+        annotationTentB.coordinate = CLLocationCoordinate2D(latitude: 48.358860, longitude: 10.906377)
+        
+        addAnnotation(annotationL)
+        addAnnotation(annotationM)
+        addAnnotation(annotationW)
+        addAnnotation(annotationTentA)
+        addAnnotation(annotationTentB)
+    }
+    
+    private func createOverlay() {
+        let coordinatesL = [
+            CLLocationCoordinate2D(latitude: 48.35875, longitude: 10.90542),
+            CLLocationCoordinate2D(latitude: 48.35923, longitude: 10.90647),
+            CLLocationCoordinate2D(latitude: 48.35917, longitude: 10.90653),
+            CLLocationCoordinate2D(latitude: 48.35916, longitude: 10.90650),
+            CLLocationCoordinate2D(latitude: 48.35912, longitude: 10.90653),
+            CLLocationCoordinate2D(latitude: 48.35866, longitude: 10.90552)
+        ]
+        
+        let coordinatesK = [
+            CLLocationCoordinate2D(latitude: 48.35923, longitude: 10.90650),
+            CLLocationCoordinate2D(latitude: 48.35928, longitude: 10.90659),
+            CLLocationCoordinate2D(latitude: 48.35906, longitude: 10.90688),
+            CLLocationCoordinate2D(latitude: 48.35894, longitude: 10.90714),
+            CLLocationCoordinate2D(latitude: 48.35888, longitude: 10.90703),
+            CLLocationCoordinate2D(latitude: 48.35901, longitude: 10.90677)
+        ]
+        
+        let coordinatesM = [
+            CLLocationCoordinate2D(latitude: 48.35833, longitude: 10.90582),
+            CLLocationCoordinate2D(latitude: 48.35893, longitude: 10.90685),
+            CLLocationCoordinate2D(latitude: 48.35890, longitude: 10.90690),
+            CLLocationCoordinate2D(latitude: 48.35892, longitude: 10.90694),
+            CLLocationCoordinate2D(latitude: 48.35887, longitude: 10.90700),
+            CLLocationCoordinate2D(latitude: 48.35884, longitude: 10.90696),
+            CLLocationCoordinate2D(latitude: 48.35878, longitude: 10.90705),
+            CLLocationCoordinate2D(latitude: 48.35849, longitude: 10.90656),
+            CLLocationCoordinate2D(latitude: 48.35856, longitude: 10.90647),
+            CLLocationCoordinate2D(latitude: 48.35841, longitude: 10.90621),
+            CLLocationCoordinate2D(latitude: 48.35838, longitude: 10.90625),
+            CLLocationCoordinate2D(latitude: 48.35825, longitude: 10.90604),
+            CLLocationCoordinate2D(latitude: 48.35828, longitude: 10.90599),
+            CLLocationCoordinate2D(latitude: 48.35825, longitude: 10.90593)
         ]
         
         let coordinatesW = [
-            CLLocationCoordinate2D(latitude: 48.358850, longitude: 10.907276),
-            CLLocationCoordinate2D(latitude: 48.358623, longitude: 10.907593),
-            CLLocationCoordinate2D(latitude: 48.358228, longitude: 10.906906),
-            CLLocationCoordinate2D(latitude: 48.358460, longitude: 10.906607)
+            CLLocationCoordinate2D(latitude: 48.35844, longitude: 10.90664),
+            CLLocationCoordinate2D(latitude: 48.35881, longitude: 10.90727),
+            CLLocationCoordinate2D(latitude: 48.35861, longitude: 10.90754),
+            CLLocationCoordinate2D(latitude: 48.35824, longitude: 10.90690)
         ]
         
         let coordinatesTentA = [
@@ -70,24 +112,37 @@ class MapViewOverlays: MKMapView, MKMapViewDelegate {
             CLLocationCoordinate2D(latitude: 48.358676, longitude: 10.906307)
         ]
         
-        let polygonLM = MKPolygon(coordinates: coordinatesLM, count: coordinatesLM.count)
+        let polygonL = MKPolygon(coordinates: coordinatesL, count: coordinatesL.count)
+        let polygonK = MKPolygon(coordinates: coordinatesK, count: coordinatesK.count)
+        let polygonM = MKPolygon(coordinates: coordinatesM, count: coordinatesM.count)
         let polygonW = MKPolygon(coordinates: coordinatesW, count: coordinatesW.count)
         let polygonTentA = MKPolygon(coordinates: coordinatesTentA, count: coordinatesTentA.count)
         let polygonTentB = MKPolygon(coordinates: coordinatesTentB, count: coordinatesTentB.count)
         
-        addOverlay(polygonLM)
+        addOverlay(polygonL)
+        addOverlay(polygonK)
+        addOverlay(polygonM)
         addOverlay(polygonW)
         addOverlay(polygonTentA)
         addOverlay(polygonTentB)
     }
     
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        let polygonRenderer = MKPolygonRenderer(overlay: overlay)
-        polygonRenderer.fillColor = PyramidColor.pyramidBrighterBlue.withAlphaComponent(0.3)
-        polygonRenderer.strokeColor = PyramidColor.pyramidDarkBlue
-        polygonRenderer.lineWidth = 2
+    func createTentWaypoint() {
+        let coordinates = [
+            CLLocationCoordinate2D(latitude: 48.35554, longitude: 10.90401),
+            CLLocationCoordinate2D(latitude: 48.35664, longitude: 10.90633),
+            CLLocationCoordinate2D(latitude: 48.35689, longitude: 10.90601),
+            CLLocationCoordinate2D(latitude: 48.35752, longitude: 10.90442),
+            CLLocationCoordinate2D(latitude: 48.35782, longitude: 10.90435),
+            CLLocationCoordinate2D(latitude: 48.35785, longitude: 10.90506),
+            CLLocationCoordinate2D(latitude: 48.35796, longitude: 10.9053),
+            CLLocationCoordinate2D(latitude: 48.35832, longitude: 10.90531),
+            CLLocationCoordinate2D(latitude: 48.35832, longitude: 10.90573)
+        ]
         
-        return polygonRenderer
+        let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+        
+        addOverlay(polyline)
     }
     
 }
