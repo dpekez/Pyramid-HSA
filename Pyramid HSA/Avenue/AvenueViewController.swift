32d3395548480a8dada2fbcc265b3b6412d26151
Dejan Pekez <dejan@pekez.de>
2019-04-04 22:11:50 +0200

    ads gestures

--- Pyramid HSA/Avenue/AvenueViewController.swift
+++ Pyramid HSA/Avenue/AvenueViewController.swift
@@ -10,41 +10,21 @@ import UIKit
 import MapKit
 
 class AvenueViewController: UIViewController {
-    @IBOutlet weak var mapView: MapViewOverlays!
-    @IBAction func centerMapButton(_ sender: UIButton) {
-        setMapCamera()
-    }
-    let gMaps = "https://goo.gl/maps/bda4HnWPQ5G2"
-    let aMaps = "https://maps.apple.com/?q=48.358685,10.907935&sll=48.358685,10.907935&sspn=0.001922,0.003246&t=m"
-    
-    @IBAction func navButton(_ sender: UIBarButtonItem) {
-        let actionSheet = UIAlertController(title: nil, message: "Zur Firmenregistrierung navigieren", preferredStyle: .actionSheet)
-        
-        // make sure this works on iPad too
-        if let actionSheet = actionSheet.popoverPresentationController {
-            actionSheet.barButtonItem = sender
-        }
-        
-        let gMapsAction = UIAlertAction(title: "In Google Maps öffnen", style: .default, handler: {_ in self.openURL(string: self.gMaps)})
-        let aMapsAction = UIAlertAction(title: "In Apple Maps öffnen", style: .default, handler: {_ in self.openURL(string: self.aMaps)})
-        actionSheet.addAction(gMapsAction)
-        actionSheet.addAction(aMapsAction)
-        actionSheet.addAction(UIAlertAction(title: "Abbrechen", style: .cancel))
-        self.present(actionSheet, animated: true)
-    }
-    
-    @IBAction func buildingButton(_ sender: UIButton) {
-    }
-    
-    @IBAction func tentButton(_ sender: UIButton) {
-    }
-    
     let locationManager = CLLocationManager()
+    let gMaps = "https://www.google.de/maps/place/Hochschule+Augsburg/@48.3589748,10.9063944,18z/data=!4m5!3m4!1s0x0:0x8a16b7655d3bfdc5!8m2!3d48.3583307!4d10.9058189"
+    let aMaps = "https://maps.apple.com/?q=48.359260,10.906433&sll=48.359260,10.906433&sspn=0.001197,0.002547&t=m"
+    let gMapsRegistration = ""
+    let aMapsRegistration = ""
+    let nearDist = CLLocationDistance(700)
+    
+    @IBOutlet weak var mapView: MapViewOverlays!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setMapCamera()
         mapView.createRegistrationPointAnnotation()
+        mapView.createParkingAnnotation()
+        mapView.createPublicTransportAnnotation()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -53,18 +33,81 @@ class AvenueViewController: UIViewController {
     }
     
     private func setMapCamera() {
-        let location = CLLocationCoordinate2D(latitude: 48.3578, longitude: 10.9059)
-        let distance = CLLocationDistance(1050)
-        let pitch = CGFloat(0)
-        let heading = CLLocationDirection(0)
-        
-        let mapCam = MKMapCamera(lookingAtCenter: location, fromDistance: distance, pitch: pitch, heading: heading)
-        
-        mapView.setCamera(mapCam, animated: true)
+        let loc = CLLocationCoordinate2D(latitude: 48.3559, longitude: 10.9080)
+        let dist = CLLocationDistance(2500)
+    
+        mapView.moveCamera(to: loc, atDistance: dist)
     }
+    
+    @IBAction func centerMapButton(_ sender: UIButton) {
+        setMapCamera()
+    }
+    
+    @IBAction func navButton(_ sender: UIBarButtonItem) {
+        let actionSheet = UIAlertController(title: nil, message: "Friedberger Str. 2, 86161 Augsburg", preferredStyle: .actionSheet)
+        
+        // make sure this works on iPad too
+        if let actionSheet = actionSheet.popoverPresentationController {
+            actionSheet.barButtonItem = sender
+        }
+        
+        let gMapsAction = UIAlertAction(title: "In „Google Maps“ öffnen", style: .default, handler: {_ in self.openURL(string: self.gMaps)})
+        let aMapsAction = UIAlertAction(title: "In „Karten“ öffnen", style: .default, handler: {_ in self.openURL(string: self.aMaps)})
+        actionSheet.addAction(gMapsAction)
+        actionSheet.addAction(aMapsAction)
+        actionSheet.addAction(UIAlertAction(title: "Abbrechen", style: .cancel))
+        self.present(actionSheet, animated: true)
+    }
+    
+    @IBAction func tramSixTapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.3588, longitude: 10.9065)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+    
+    @IBAction func bus32Tapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.3588, longitude: 10.9065)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+    
+    @IBAction func bus35Tapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.3588, longitude: 10.9065)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+    
+    @IBAction func tramTwoTapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.358190, longitude: 10.902142)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+    
+    @IBAction func tramThreeTapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.358190, longitude: 10.902142)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+    
+    @IBAction func trainTapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.355013, longitude: 10.901830)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+    
+    @IBAction func parkingFrischTapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.352311, longitude: 10.906202)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+    
+    @IBAction func parkingSpickelTapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.355245, longitude: 10.914821)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+
+    @IBAction func registrationTapped(_ sender: UITapGestureRecognizer) {
+        let loc = CLLocationCoordinate2D(latitude: 48.35867, longitude: 10.90793)
+        mapView.moveCamera(to: loc, atDistance: nearDist)
+    }
+    
 }
 
 extension AvenueViewController {
+    
     func checkLocationAuthorizationStatus() {
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             mapView.showsUserLocation = true
@@ -72,9 +115,11 @@ extension AvenueViewController {
             locationManager.requestWhenInUseAuthorization()
         }
     }
+    
 }
 
 extension AvenueViewController {
+    
     private func openURL(string: String) {
         let link = URL(string: string)
         if link != nil {
@@ -83,12 +128,11 @@ extension AvenueViewController {
             showURLFailAlert()
         }
     }
-}
-
-extension AvenueViewController {
+    
     private func showURLFailAlert() {
         let alert = UIAlertController(title: "Hoppla!", message: "Leider ist der Link nicht gültig.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         self.present(alert, animated: true)
     }
+    
 }
