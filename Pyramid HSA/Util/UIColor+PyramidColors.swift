f9e6ff66683f30d0d3bda6c510c8dccde2ee4fc2
Dejan Pekez <dejan@pekez.de>
2019-04-04 20:57:50 +0200

    pyramid colors are now an extension to uicolor

---
+++ Pyramid HSA/Util/UIColor+PyramidColors.swift
@@ -0,0 +1,72 @@
+//
+//  UIColor+PyramidColors.swift
+//  Pyramid HSA
+//
+//  Created by Dejan Pekez on 21.12.18.
+//  Copyright © 2018 Dejan Pekez. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var customGrey: UIColor {
+        return UIColor(white: 0.7, alpha: 0.3)
+    }
+    
+    // #C5C8E6
+    static var pyramidBrighterBlue: UIColor {
+        return UIColor(red:0.77, green:0.78, blue:0.90, alpha:1.0)
+    }
+    
+    // #9FA7D4
+    static var pyramidBrightBlue: UIColor {
+        return UIColor(red:0.62, green:0.65, blue:0.83, alpha:1.0)
+    }
+    
+    // #6F81BE
+    static var pyramidMidBlue: UIColor {
+        return UIColor(red:0.44, green:0.51, blue:0.75, alpha:1.0)
+    }
+    
+    // #566FB3
+    static var pyramidDarkBlue: UIColor {
+        return UIColor(red:0.34, green:0.44, blue:0.70, alpha:1.0)
+    }
+    
+    // #3D5FA8 (pyramidDarkerBlue)
+    static var pyramidBlue: UIColor {
+        return UIColor(red:0.24, green:0.37, blue:0.66, alpha:1.0)
+    }
+    
+    // #006532
+    static var architecture: UIColor {
+        return UIColor(red:0.00, green:0.40, blue:0.20, alpha:1.0)
+    }
+    
+    // #82358B
+    static var businessAdministration: UIColor {
+        return UIColor(red:0.51, green:0.21, blue:0.55, alpha:1.0)
+    }
+    
+    // #925F36
+    static var computerScience: UIColor {
+        return UIColor(red:0.57, green:0.37, blue:0.21, alpha:1.0)
+    }
+    
+    // #F39200
+    static var design: UIColor {
+        return UIColor(red:0.95, green:0.57, blue:0.00, alpha:1.0)
+    }
+    
+    // #E6332A
+    static var electricalEngineering: UIColor {
+        return UIColor(red:0.90, green:0.20, blue:0.16, alpha:1.0)
+    }
+    
+    // #3362AC
+    static var mechanicalEngineering: UIColor {
+        return UIColor(red:0.20, green:0.38, blue:0.67, alpha:1.0)
+    }
+    
+}
