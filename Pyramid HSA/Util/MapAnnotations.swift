8fa9e765d41597bd53f722220042ac169f496e0b
Dejan Pekez <dejan@pekez.de>
2019-04-08 21:46:36 +0200

    ads map annotation classes

---
+++ Pyramid HSA/Util/MapAnnotations.swift
@@ -0,0 +1,38 @@
+//
+//  MapAnnotations.swift
+//  Pyramid HSA
+//
+//  Created by Dejan Pekez on 07.04.19.
+//  Copyright © 2019 Dejan Pekez. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class BuildingAnnotation: NSObject, MKAnnotation {
+    
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var kind: FloorName
+
+    init(kind: FloorName ,title: String, latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        self.title = title
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        self.kind = kind
+    }
+    
+}
+
+class AvenueAnnotation: NSObject, MKAnnotation {
+    
+    var title: String?
+    var subtitle: String?
+    var coordinate: CLLocationCoordinate2D
+    
+    init(title: String, subtitle: String? = nil, latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        self.title = title
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        self.subtitle = subtitle
+    }
+    
+}
