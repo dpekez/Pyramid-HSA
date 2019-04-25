8dddb06dcc96c37389c735d8bdc04d3c42f10ea5
Dejan Pekez <dejan@pekez.de>
2019-04-25 15:32:02 +0200

    ads Struct for Event Data

---
+++ Pyramid HSA/Util/EventData.swift
@@ -0,0 +1,19 @@
+//
+//  EventData.swift
+//  Pyramid HSA
+//
+//  Created by Dejan Pekez on 22.04.19.
+//  Copyright © 2019 Dejan Pekez. All rights reserved.
+//
+
+import Foundation
+
+struct EventData {
+    
+    let eventHeader: String
+    let eventBody: String
+    let eventLocation: String
+    let eventImage: String
+    let eventMap: String
+    
+}
